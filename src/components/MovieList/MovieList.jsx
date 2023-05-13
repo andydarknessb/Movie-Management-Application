9c0d82a60629f4d2a60d96eeff53f37b308f3de6
Cory Anderson <cory.anderson6@yahoo.com>
2023-05-13 00:21:04 -0500

    imported link/wrapped img with the link component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { Link } from 'react-router-dom';
 
 function MovieList() {
 
@@ -13,20 +14,21 @@ function MovieList() {
 
     return (
         <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
+          <h1>MovieList</h1>
+          <section className="movies">
+            {movies.map((movie) => {
+              return (
+                <div key={movie.id}>
+                  <h3>{movie.title}</h3>
+                  <Link to={`/details/${movie.id}`}>
+                    <img src={movie.poster} alt={movie.title} />
+                  </Link>
+                </div>
+              );
+            })}
+          </section>
         </main>
-
-    );
-}
-
-export default MovieList;
+      );
+    }
+    
+    export default MovieList;
