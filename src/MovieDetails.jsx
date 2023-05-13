654973b767bf3f852073352ac6dcd45ff72e725c
Cory Anderson <cory.anderson6@yahoo.com>
2023-05-13 00:33:58 -0500

    useParams and useHistory hooks from react router dom to retrieve the movie ID from the URL and navigate back to the movie list

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -1,3 +1,18 @@
 import React, { useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+
+function MovieDetails() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { id } = useParams();
+    const movie = useSelector((store) => store.selectedMovie);
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: id });
+    }, [id]);
+
+    const goBackToList = () => {
+        history.push('/');
+    };
+}
